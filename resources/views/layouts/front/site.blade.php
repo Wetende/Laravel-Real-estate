05410301c89b1423f1869e9f8567a420d02653f7
Wetende <100572229+Wetende@users.noreply.github.com>
2022-07-21 23:25:21 +0300

    first commit

--- resources/views/layouts/front/site.blade.php
+++ resources/views/layouts/front/site.blade.php
@@ -5,19 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <!-- External CSS libraries -->
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/bootstrap.min.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/magnific-popup.css")}}>
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/jquery.selectBox.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/dropzone.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/rangeslider.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/animate.min.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/leaflet.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/slick.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/slick-theme.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/map.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/css/jquery.mCustomScrollbar.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/fonts/font-awesome/css/font-awesome.min.css")}}">
-    <link type="text/css" rel="stylesheet" href="{{assets("assets/fonts/flaticon/font/flaticon.css")}}">
+    
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/bootstrap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/magnific-popup.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/jquery.selectBox.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/dropzone.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/rangeslider.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/animate.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/leaflet.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/slick.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/slick-theme.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/map.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/css/jquery.mCustomScrollbar.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/fonts/font-awesome/css/font-awesome.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{asset('assets/front/fonts/flaticon/font/flaticon.css')}}">
+
 
     <!-- Favicon icon -->
     <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" >
@@ -65,30 +67,30 @@
 
 
 <!-- External JS libraries -->
-<script src="{{assets('assets/front/js/jquery-2.2.0.min.js')}}"></script>
-<script src="{{assets('assets/front/js/popper.min.js')}}"></script>
-<script src="{{assets('assets/front/js/bootstrap.min.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.selectBox.js')}}"></script>
-<script src="{{assets('assets/front/js/rangeslider.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.filterizr.js')}}"></script>
-<script src="{{assets('assets/front/js/wow.min.js')}}"></script>
-<script src="{{assets('assets/front/js/backstretch.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.countdown.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.scrollUp.js')}}"></script>
-<script src="{{assets('assets/front/js/particles.min.js')}}"></script>
-<script src="{{assets('assets/front/js/typed.min.js')}}"></script>
-<script src="{{assets('assets/front/js/dropzone.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.mb.YTPlayer.js')}}"></script>
-<script src="{{assets('assets/front/js/leaflet.js')}}"></script>
-<script src="{{assets('assets/front/js/leaflet-providers.js')}}"></script>
-<script src="{{assets('assets/front/js/leaflet.markercluster.js')}}"></script>
-<script src="{{assets('assets/front/js/slick.min.js')}}"></script>
-<script src="{{assets('assets/front/js/maps.js')}}"></script>
-<script src="{{assets('assets/front/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery-2.2.0.min.js')}}"></script>
+<script src="{{asset('assets/front/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/front/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.selectBox.js')}}"></script>
+<script src="{{asset('assets/front/js/rangeslider.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.filterizr.js')}}"></script>
+<script src="{{asset('assets/front/js/wow.min.js')}}"></script>
+<script src="{{asset('assets/front/js/backstretch.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.countdown.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.scrollUp.js')}}"></script>
+<script src="{{asset('assets/front/js/particles.min.js')}}"></script>
+<script src="{{asset('assets/front/js/typed.min.js')}}"></script>
+<script src="{{asset('assets/front/js/dropzone.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.mb.YTPlayer.js')}}"></script>
+<script src="{{asset('assets/front/js/leaflet.js')}}"></script>
+<script src="{{asset('assets/front/js/leaflet-providers.js')}}"></script>
+<script src="{{asset('assets/front/js/leaflet.markercluster.js')}}"></script>
+<script src="{{asset('assets/front/js/slick.min.js')}}"></script>
+<script src="{{asset('assets/front/js/maps.js')}}"></script>
+<script src="{{asset('assets/front/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0N5pbJN10Y1oYFRd0MJ_v2g8W2QT74JE"></script>
-<script src="assets/js/ie-emulation-modes-warning.js"></script>
+<script src="{{asset('assets/front/js/ie-emulation-modes-warning.js')}}"></script>
 <!-- Custom JS Script -->
-<script  src="{{assets('assets/front/js/app.js')}}"></script>
+<script  src="assets/js/app.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('front');
+    return view('front.site');
 });
 
 Auth::routes();
